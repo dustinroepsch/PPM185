71b25132ccb763e7787cdad2ab240e9ac0922ac1
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2017-12-26 02:34:40 -0600

    wrote color setters

--- ppm185.c
+++ ppm185.c
@@ -2,10 +2,10 @@
 
 #include <stdlib.h>
 
-struct ppm_rgb
+typedef struct
 {
     uint8_t r, g, b;
-};
+} ppm_rgb_t;
 
 struct ppm_image
 {
@@ -18,12 +18,38 @@ ppm_image_t *ppm_create(uint32_t width, uint32_t height)
     ppm_image_t *image = malloc(sizeof(ppm_image_t));
     image->width = width;
     image->height = height;
-    image->raster = malloc(sizeof(ppm_rgb_t) * width * height);
+    //raster is calloc'd to 0 (all black pixels)
+    image->raster = calloc(width * height, sizeof(ppm_rgb_t));
+    ;
     return image;
 }
 
-void ppm_destory(ppm_image_t **image) {
+void ppm_destory(ppm_image_t **image)
+{
     free((*image)->raster);
     free(*image);
     *image = NULL; //so students seg fault if they try to keep using image
+}
+
+void ppm_set_red(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t red)
+{
+    image->raster[row * image->width + col].r = red;
+}
+
+void ppm_set_green(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t green)
+{
+    image->raster[row * image->width + col].g = green;
+}
+
+void ppm_set_blue(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t blue)
+{
+    image->raster[row * image->width + col].b = blue;
+}
+
+void ppm_set_rgb(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t red, uint8_t green, uint8_t blue)
+{
+    ppm_rgb_t *pixel = &(image->raster[row * image->width + col]);
+    pixel->r = red;
+    pixel->g = green;
+    pixel->b = blue;
 }
--- ppm185.h
+++ ppm185.h
@@ -3,8 +3,6 @@
 
 #include <stdint.h>
 
-typedef struct ppm_rgb ppm_rgb_t;
-
 typedef struct ppm_image ppm_image_t;
 
 /*
@@ -40,15 +38,28 @@ ppm_image_t *ppm_create(uint32_t width, uint32_t height);
 */
 void ppm_destory(ppm_image_t **image);
 
+/*
+* Colors are represented by three numbers in the range 0-255. (unsigned 8-bit numbers)
+* We call these numbers `channels` and use them to say how much red, green, and blue color is at a given pixel in the image.
+* If want to say that a channel doesn't contribute to the color of an image at a pixel, we set that channels number
+* value to 0. Conversely, if we want that channel to contribute as much as possible to the pixel, we set it to 255.
+* Hence, the color (0, 0, 0) is black and the color (255, 255, 255) is white.
+*
+*EXAMPLE USE:
+* ppm_image_t *my_image = ppm_create(300, 400);
+* ppm_set_red(my_image, 4, 15, 180); //sets the red value for the pixel at row 4, col 15 to 180
+* ppm_set_rgb(my_image, 10, 20, 255, 255, 255); //sets pixel (10, 20) to white by setting red, green, and blue to 255
+*
+*IMPORTANT NOTES:
+* A nice tool to help you pick RGB values can be found by googling `color picker`
+* https://www.google.com/search?q=color+picker
+*
+* This tool is made by Google and let's you easily find r, g, and b values for the color you want.
+*/
 void ppm_set_red(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t red);
-
 void ppm_set_green(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t green);
-
-void ppm_set_blue(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t red);
-
-void ppm_set_rgb(ppm_image_t *image, uint32_t row, uint32_t col, ppm_rgb_t rgb);
-
-ppm_rgb_t ppm_get_rgb(ppm_image_t *image, uint32_t row, uint32_t col);
+void ppm_set_blue(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t blue);
+void ppm_set_rgb(ppm_image_t *image, uint32_t row, uint32_t col, uint8_t red, uint8_t green, uint8_t blue);
 
 void ppm_write_to_file(char* filename);
 
